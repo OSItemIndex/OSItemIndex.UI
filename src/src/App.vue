da14578ec6c8cabd24e04fb86aaca7eba7235531
Twinki <thetwinkehh@gmail.com>
2021-06-10 07:04:57 -0500

    wip: move to vue 2, with vuetify

---
+++ src/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <AppBar />
+    <v-main>
+      <HelloWorld />
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import AppBar from "./components/AppBar.vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default Vue.extend({
+  name: "App",
+
+  components: {
+    AppBar,
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+});
+</script>
--- src/src/main.ts
+++ src/src/main.ts
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import "roboto-fontface/css/roboto/roboto-fontface.css";
+import "@mdi/font/css/materialdesignicons.css";
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false;
+
+new Vue({
+  vuetify,
+  render: (h) => h(App),
+}).$mount("#app");
