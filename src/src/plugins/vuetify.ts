d314d81b0146ff45f46eae2a5bf7877f585492ec
Twinki <thetwinkehh@gmail.com>
2021-06-17 05:20:37 -0500

    wip: add kofi icon component

--- src/src/plugins/vuetify.ts
+++ src/src/plugins/vuetify.ts
@@ -1,9 +1,17 @@
 import Vue from "vue";
 import Vuetify from "vuetify";
+import KofiIcon from "@/components/icons/KofiIcon.vue";
 import "vuetify/dist/vuetify.min.css";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
-    theme: { dark: true },
+  theme: { dark: true },
+  icons: {
+    values: {
+      kofi: {
+        component: KofiIcon,
+      },
+    },
+  },
 });
